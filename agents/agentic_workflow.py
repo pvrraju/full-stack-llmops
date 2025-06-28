e12eded3fffd441509d0b14aeaa88de10258aaa1
vijay <51013733+pvrraju@users.noreply.github.com>
2025-06-27 18:00:03 -0700

    code added

--- agents/agentic_workflow.py
+++ agents/agentic_workflow.py
@@ -13,9 +13,27 @@ class GraphBuilder:
         pass
 
     def agent_function(self):
+        """Main agent function"""
+        user_question = state['messages']
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages" : [response]}
+
         pass
 
     def build_graph(self):
+
+        graph_builder = StateGraph(MessagesState)
+
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START, "agent")
+        graph_builder.add_conditional_edges("agent", tools_condition)
+        graph_builder.add_edge('tools', 'agent')
+        graph_builder.add_edge('agent', END)
+        self.graph = graph_builder.compile()
+        return self.graph
+
         pass
 
     def __call__(self):
